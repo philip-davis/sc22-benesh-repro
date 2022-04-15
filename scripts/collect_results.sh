410891559a338cf10ad79cfcfa94d3faba7373be
Philip Davis <philipdavis01@gmail.com>
2022-04-15 03:04:18 -0400

    Update rank size lists

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -10,7 +10,7 @@ export REPRO_DIR=/gpfs/alpine/scratch/pdavis/fus123/benesh-heateq/repro/sc22-ben
 for method in async lockstep ; do
     for scaling in strong weak ; do
         mkdir -p ${REPRO_DIR}/results/${method}/${scaling}
-        for size in 64 ; do
+        for size in 64 256 1024 4096 ; do
             echo collecting results for $method, $scaling scaling, $size processes per component
             cd ${REPRO_DIR}/run/${method}/${scaling}/${size}
             ${REPRO_DIR}/scripts/build_csvs.sh ${size}
