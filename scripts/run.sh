9f881604478d89686b075abf0ed3cf9a00e459f2
Philip Davis <philipdavis01@gmail.com>
2022-04-14 21:47:13 -0400

    fix run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -17,7 +17,7 @@ if [ "$method" == "lockstep" ] ; then
     adhoc_exe=/benesh/benesh/build/bin/heateq_adhoc
 else
     adhoc_exe=/benesh/benesh/build/bin/heateq_adhoc_async
-if
+fi
 
 scaling=$3
 if [ "$scaling" == "strong" ] ; then
@@ -30,8 +30,10 @@ else
     ydim=$(($yprocdim * 1024))
 fi
 
+sif_file=${REPRO_DIR}/containers/sc22.sif
+
 mkdir -p $nprocs
-cp ${REPRO_DIR}/conf/heateq2d_${method}.xc $nprocs/
+cp ${REPRO_DIR}/conf/heateq2d_${method}.xc $nprocs/heateq2d.xc
 cd $nprocs
 
 rm -rf conf.ds *.ekt *.log
@@ -61,7 +63,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export TRACEDIR=dspaces.b.${iter}
     export APEX_OUTPUT_FILE_PATH=${TRACEDIR}
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
-    jsrun -n $nserv -a 1 -c 32 -r 1 -e prepended -o server-b.${iter}.out.%j -k  server-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif /install/dspaces/bin/dspaces_server verbs &
+    jsrun -n $nserv -a 1 -c 32 -r 1 -e prepended -o server-b.${iter}.out.%j -k  server-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} /install/dspaces/bin/dspaces_server verbs &
     while [ ! -f conf.ds ]; do
         sleep 1s
     done
@@ -72,7 +74,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
     export IBRUN_TASKS_PER_NODE=32
     startproc=$(($IBRUN_TASKS_PER_NODE * $nserv))
-    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o left-b.${iter}.out.%j -k left-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif /benesh/benesh/build/bin/heateq_benesh $xprocdim $yprocdim $xdim $ydim heat1 &
+    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o left-b.${iter}.out.%j -k left-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} /benesh/benesh/build/bin/heateq_benesh $xprocdim $yprocdim $xdim $ydim heat1 &
     echo "started left side"
     leftproc=$!
     mkdir bprof-right.${iter}
@@ -81,7 +83,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
     export IBRUN_TASKS_PER_NODE=32
     startproc=$(($nprocs + ($IBRUN_TASKS_PER_NODE * $nserv)))
-    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o right-b.${iter}.out.%j -k right-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif /benesh/benesh/build/bin/heateq_benesh $xprocdim $yprocdim $xdim $ydim heat2
+    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o right-b.${iter}.out.%j -k right-b.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} /benesh/benesh/build/bin/heateq_benesh $xprocdim $yprocdim $xdim $ydim heat2
     echo "finished right side, waiting on left"
     wait $leftproc
     echo "benesh iteration ${iter} complete"
@@ -90,7 +92,7 @@ for iter in `seq 1 ${ntrials}` ; do
 #for iter in `seq 1 ${ntrials}` ; do
     ## Create dataspaces configuration file
     echo "## Config file for DataSpaces
-    ndim = 
+    ndim = 2
     dims = ${xconfdim}, ${xdim}
     max_versions = 10
     num_apps = 2
@@ -104,7 +106,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export APEX_OUTPUT_FILE_PATH=${TRACEDIR}
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
     export IBRUN_TASKS_PER_NODE=1
-    jsrun -n $nserv -a 1 -c 32 -r 1 -e prepended -o server-a.${iter}.out.%j -k  server-a.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif /install/dspaces/bin/dspaces_server verbs &
+    jsrun -n $nserv -a 1 -c 32 -r 1 -e prepended -o server-a.${iter}.out.%j -k  server-a.err.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} /install/dspaces/bin/dspaces_server verbs &
     while [ ! -f conf.ds ]; do
         sleep 1s
     done
@@ -115,7 +117,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
     export IBRUN_TASKS_PER_NODE=32
     startproc=$(($IBRUN_TASKS_PER_NODE * $nserv))
-    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o left-a.${iter}.out.%j -k left-a.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif ${adhoc_exe} $xprocdim $yprocdim 0 0 0.625 1.09375 ${xdim} ${ydim} 0.46875 1.09375 &
+    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o left-a.${iter}.out.%j -k left-a.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} ${adhoc_exe} $xprocdim $yprocdim 0 0 0.625 1.09375 ${xdim} ${ydim} 0.46875 1.09375 &
     echo "started left side"
     leftproc=$!
     mkdir aprof-right.${iter}
@@ -124,7 +126,7 @@ for iter in `seq 1 ${ntrials}` ; do
     export APEX_OTF2_ARCHIVE_PATH=${TRACEDIR}
     export IBRUN_TASKS_PER_NODE=32
     startproc=$(($nprocs + ($IBRUN_TASKS_PER_NODE * $nserv)))
-    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o right-a.${iter}.out.%j -k right-a.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ../sc22.sif ${adhoc_exe} $xprocdim $yprocdim 0.46875 0 1.09375 1.09375 ${xdim} ${ydim} 0 0.625
+    jsrun -n $nprocs -a 1 -c 1 -r 32 -e prepended -o right-a.${iter}.out.%j -k right-a.${iter}.%j singularity exec --bind $MPI_ROOT:$MPI_ROOT,/autofs/nccs-svm1_home1,/autofs/nccs-svm1_home1:/ccs/home,$(pwd):/jobdir --pwd /jobdir ${sif_file} ${adhoc_exe} $xprocdim $yprocdim 0.46875 0 1.09375 1.09375 ${xdim} ${ydim} 0 0.625
     echo "finished right side, waiting on left"
     wait $leftproc
     echo "adhoc iteration ${iter} complete"
